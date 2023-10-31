20d00f9cf379671f4b33f9c24fcfb73d37c39be8
Gabriel Henrique <gabhenrique0@gmail.com>
2023-10-30 21:37:48 -0300

    Refactor: remove default content view and add a framework grid view

---
+++ Apple-Frameworks/Views/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Gabriel Pereira on 30/10/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
