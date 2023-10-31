1d9783682d44b67b20b147051fd4f96d6009471b
Gabriel Henrique <gabhenrique0@gmail.com>
2023-10-30 22:45:02 -0300

    Feature: add a LazyVGrid on FrameworkGridView and iterate using a ForEach on mock data to display the list of frameworks

--- Apple-Frameworks/Views/FrameworkGridView.swift
+++ Apple-Frameworks/Views/FrameworkGridView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        Text("Hello, World!")
+        LazyVGrid(columns: columns) {
+            ForEach(MockData.frameworks, id: \.id) { framework in
+                FrameworkTitleView(framework: framework)
+            }
+        }
     }
 }
 
