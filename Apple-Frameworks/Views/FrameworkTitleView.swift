d85a1350a524a18c538443faa1e5a2dbc8ee0e4a
Gabriel Henrique <gabhenrique0@gmail.com>
2023-10-30 22:44:28 -0300

    Feature: create a view to display the framework title view on app

---
+++ Apple-Frameworks/Views/FrameworkTitleView.swift
@@ -0,0 +1,34 @@
+//
+//  FrameworkTitleView.swift
+//  Apple-Frameworks
+//
+//  Created by Gabriel Pereira on 30/10/23.
+//
+
+import SwiftUI
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(framework.name)
+                .font(.title)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
+
+struct FrameworkTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        let framework = MockData.frameworks.first!
+        
+        FrameworkTitleView(framework: framework)
+    }
+}
