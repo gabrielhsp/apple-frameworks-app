2f46ecfacbd0ce245407ec8dbc83fddc61e45498
Gabriel Henrique <gabhenrique0@gmail.com>
2023-11-20 21:29:53 -0300

    Refactor: replace navigation view with a navigation stack, adding a destination instead of using a sheet

--- Apple-Frameworks/Scenes/FrameworkGridView/FrameworkGridView.swift
+++ Apple-Frameworks/Scenes/FrameworkGridView/FrameworkGridView.swift
@@ -11,21 +11,19 @@ struct FrameworkGridView: View {
     @StateObject var viewModel = FrameworkGridViewModel()
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ScrollView {
                 LazyVGrid(columns: viewModel.columns) {
                     ForEach(MockData.frameworks, id: \.id) { framework in
-                        FrameworkTitleView(framework: framework)
-                            .onTapGesture {
-                                viewModel.selectedFramework = framework
-                            }
+                        NavigationLink(value: framework) {
+                            FrameworkTitleView(framework: framework)
+                        }
                     }
                 }                
             }
             .navigationTitle("🍎 Frameworks")
-            .sheet(isPresented: $viewModel.isShowingDetailView) {
-                FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework,
-                                    isShowingDetailView: $viewModel.isShowingDetailView)
+            .navigationDestination(for: Framework.self) { framework in
+                FrameworkDetailView(framework: framework)
             }
         }
     }
