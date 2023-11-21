d86f5f89da7b290a193a473d1194933d74ab6e1b
Gabriel Henrique <gabhenrique0@gmail.com>
2023-11-20 21:28:57 -0300

    Refactor: remove dismiss button, spacer and is showing detail view binding property

--- Apple-Frameworks/Scenes/FrameworkDetailView/FrameworkDetailView.swift
+++ Apple-Frameworks/Scenes/FrameworkDetailView/FrameworkDetailView.swift
@@ -9,15 +9,10 @@ import SwiftUI
 
 struct FrameworkDetailView: View {
     let framework: Framework
-    @Binding var isShowingDetailView: Bool
     @State private var isShowingSafariView: Bool = false
     
     var body: some View {
         VStack {
-            DismissButton(isShowingDetailView: $isShowingDetailView)
-            
-            Spacer()
-            
             FrameworkTitleView(framework: framework)
             
             Text(framework.description)
@@ -40,6 +35,6 @@ struct FrameworkDetailView: View {
 
 struct FrameworkDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
+        FrameworkDetailView(framework: MockData.sampleFramework)
     }
 }
